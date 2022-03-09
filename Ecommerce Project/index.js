6b1fb695a20ced4388fd91640df83e48cccf1767
subash <adhiksubash@gmail.com>
2022-03-09 20:07:25 +0545

    login route and view completed

--- Ecommerce Project/index.js
+++ Ecommerce Project/index.js
@@ -3,14 +3,24 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 import user from "./routes/user.js";
 import db from "./service/database.js";
+
 const app = express();
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, () => {
   db();
   console.log(`Server started at port 3000`);
 });
 
+//api routes
 app.use(user);
 
-// todo : create login route
+// ui routes
+app.get("/", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/index", (req, res) => {
+  res.render("index.ejs");
+});
